26c954ba5e7575907b1edf4c8da32b5f35996728
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2021-01-16 23:31:30 +0100

    Added "unclickable buttons" if recipe can't be crafted

--- Assets/_Game/Scripts/Runtime/Crafting/CraftingListUI/CraftableItemUI.cs
+++ Assets/_Game/Scripts/Runtime/Crafting/CraftingListUI/CraftableItemUI.cs
@@ -1,3 +1,4 @@
+using Shopkeeper.World;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,6 +13,8 @@ namespace Shopkeeper.Crafting
         [SerializeField] private Image result = null;
         [SerializeField] private Button addButton = null;
         private CraftingRecipe recipe;
+        private CanvasGroup buttonCanvasGroup;
+        private CraftingMaterials materials;
 
         public event Action<CraftingRecipe> RecipeSelected;
 
@@ -22,6 +25,22 @@ namespace Shopkeeper.Crafting
             result.sprite = recipe.Result.Sprite;
 
             addButton.onClick.AddListener(OnAddButton);
+
+            addButton.TryGetComponent(out buttonCanvasGroup);
+
+            materials = WorldContext.PlayerState.CraftingMaterials;
+            materials.Changed += (i, c) => UpdateButtonState();
+            UpdateButtonState();
+        }
+
+        private void UpdateButtonState()
+        {
+            bool canCraft = materials.Contains(recipe.Ingredients);
+            if (buttonCanvasGroup != null)
+            {
+                buttonCanvasGroup.alpha = canCraft ? 1 : 0.25f;
+            }
+            addButton.interactable = canCraft;
         }
 
         public void OnAddButton()
